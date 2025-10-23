f06d9c073a3b5fa43d27ac714fa16c2999a76501
mmiiffii <mifidj@me.com>
2025-10-23 12:16:42 +0200

    Update generate_timelapse_last3days.py

--- scripts/generate_timelapse_last3days.py
+++ scripts/generate_timelapse_last3days.py
@@ -39,7 +39,7 @@ LONGITUDE = -3.3920
 TIMEZONE = "Europe/Madrid"
 
 # How many minutes before sunrise to start
-MINUTES_BEFORE_SUNRISE = 100
+MINUTES_BEFORE_SUNRISE = 300
 
 # Bad-frame detection thresholds (tweak if needed)
 MIN_DIM = 100
